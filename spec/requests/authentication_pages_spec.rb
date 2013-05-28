d92d4800820787e9a6853f3b697f03cedf5e9697
Krish <jayaram.krish@gmail.com>
2013-05-28 14:48:29 -0400

    Add user following

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -53,76 +53,91 @@ describe "Authentication" do
 	describe "authorization" do
 
 
-		describe "as non-admin user" do
-			let(:user) {FactoryGirl.create(:user)}
-			let(:non_admin) {FactoryGirl.create(:user)}
+			describe "as non-admin user" do
+				let(:user) {FactoryGirl.create(:user)}
+				let(:non_admin) {FactoryGirl.create(:user)}
 
-			before {sign_in non_admin}
+				before {sign_in non_admin}
 
-			describe "submitting a DELETE request to the Users#destroy action" do
-				before {delete user_path(user)}
-				specify { response.should redirect_to(root_path)}
+				describe "submitting a DELETE request to the Users#destroy action" do
+					before {delete user_path(user)}
+					specify { response.should redirect_to(root_path)}
+				end
 			end
-		end
 
-		describe "for non-signed-in users" do
-	      let(:user) { FactoryGirl.create(:user) }
+			describe "for non-signed-in users" do
+		      let(:user) { FactoryGirl.create(:user) }
 
-	      	describe "when attempting to visit a protected page" do
-	       		 before do
-			          visit edit_user_path(user)
-			          fill_in "Email",    with: user.email
-			          fill_in "Password", with: user.password
-			          click_button "Sign in"
+		      describe "in the Users controller" do
+
+					describe "visiting the edit page" do
+							before { visit edit_user_path(user)}
+							it {should have_selector('title', text: 'Sign in')}
+					end
+
+					describe "submitting to the update action" do
+							before { put user_path(user)}
+							specify { response.should redirect_to(signin_path)}
+					end
+
+					describe "visiting the user index" do
+						before {visit users_path}
+						it {should have_selector('title', text: 'Sign in')}
+					end
+
+					describe "visiting the following page" do
+		          		before { visit following_user_path(user) }
+		          		it { should have_selector('title', text: 'Sign in') }
+		        	end
+
+		        	describe "visiting the followers page" do
+		         		before { visit followers_user_path(user) }
+		          		it { should have_selector('title', text: 'Sign in') }
+		        	end
+				end
+
+		      	describe "when attempting to visit a protected page" do
+			       		 before do
+					          visit edit_user_path(user)
+					          fill_in "Email",    with: user.email
+					          fill_in "Password", with: user.password
+					          click_button "Sign in"
+					        end
+
+			        describe "after signing in" do
+
+			   		       it "should render the desired protected page" do
+			        		    page.should have_selector('title', text: 'Edit user')
+			          		end
 			        end
 
-	        describe "after signing in" do
+			        describe "in the Microposts controller" do
 
-	   		       it "should render the desired protected page" do
-	        		    page.should have_selector('title', text: 'Edit user')
-	          		end
-	        end
+			        	describe "submitting to the create action" do
+			        		before {post microposts_path}
+			        		specify { response.should redirect_to(signin_path)}
+			        	end
 
-	        describe "in the Microposts controller" do
+			        	describe "submitting to the destroy action" do
+			        		before {delete micropost_path(FactoryGirl.create(:micropost))}
+			        		specify { response.should redirect_to(signin_path)}
+			        	end
+			        end
+				
+					describe "in the Relationships controller" do
+						describe "submitting to the create action" do
+							before { post relationships_path}
+							specify {response.should redirect_to(signin_path)}
+						end
 
-	        	describe "submitting to the create action" do
-	        		before {post microposts_path}
-	        		specify { response.should redirect_to(signin_path)}
-	        	end
+						describe "submitting to the destroy action" do
+							before { delete relationship_path(1)}
+							specify {response.should redirect_to(signin_path)}
+						end
+					end
+				end
 
-	        	describe "submitting to the destroy action" do
-	        		before {delete micropost_path(FactoryGirl.create(:micropost))}
-	        		specify { response.should redirect_to(signin_path)}
-	        	end
-	        end
-
-	 	end
-
-
-		describe "in the Users controller" do
-
-			describe "visiting the edit page" do
-					before { visit edit_user_path(user)}
-					it {should have_selector('title', text: 'Sign in')}
 			end
-
-			describe "submitting to the update action" do
-					before { put user_path(user)}
-					specify { response.should redirect_to(signin_path)}
-			end
-
-			describe "visiting the user index" do
-				before {visit users_path}
-				it {should have_selector('title', text: 'Sign in')}
-			end
-
-
-		end
-
-
-
-
-	end
 
 
 		describe "as wrong user" do
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -16,6 +16,36 @@ describe "UserPages" do
   	it {should have_selector('title', text: 'All users')}
   	it {should have_selector('h1', text: 'All users')}
 
+  	describe "following/followers" do
+  		let(:user) {FactoryGirl.create(:user)}
+  		let(:other_user) {FactoryGirl.create(:user)}
+  		before {user.follow!(other_user)}
+
+  		describe "followed users" do
+  			before do
+  				sign_in user
+  				visit following_user_path(user)
+  			end
+
+  			it {should have_selector('title', text: full_title('Following'))}
+  			it {should have_selector('h3', text: 'Following')}
+  			it {should have_link(other_user.name, href: user_path(other_user))}
+  		end
+
+  		describe "followers" do
+  			before do
+  				sign_in user
+  				visit followers_user_path(other_user)
+  			end
+
+  			it {should have_selector('title', text: full_title('Followers'))}
+  			it {should have_selector('h3', text: 'Followers')}
+  			it {should have_link(user.name, href: user_path(user))}
+  		end
+  	end
+
+
+
   	describe "pagination" do
   		before(:all) {30.times {FactoryGirl.create(:user)}}
   		after(:all) {User.delete_all}
@@ -129,9 +159,64 @@ describe "UserPages" do
 	   	it {should have_content(m1.content)}
 	   	it {should have_content(m2.content)}
 	   	it {should have_content(user.microposts.count)}
+
+	   	# describe "should have the correct count of microposts" do
+	   	# 	#let! (:correct_micropost_count) { view.pluralize(user.microposts.count, "micropost") }
+	   	# 	#it {should have_content(correct_micropost_count)}
+	   	# 	it {should have_content(view.pluralize(user.microposts.count, "micropost"))}
+	   	# end
 	   end
 
-   end
+	   describe "follow/unfollow buttons" do
+		   	let(:other_user) {FactoryGirl.create(:user)}
+		   	before {sign_in user}
+
+		   	describe "following a user" do
+		   		before {visit user_path(other_user)}
+
+		   		it "should increment the followed user count" do
+		   			expect do
+		   				click_button "Follow"
+		   			end.to change(user.followed_users, :count).by(1)
+		   		end
+
+		   		it "should increment the other user's followers count" do
+		   			expect do
+		   				click_button "Follow"
+		   			end.to change(other_user.followers, :count).by(1)
+		   		end
+
+		   		describe "toggling the button" do
+		   			before {click_button "Follow"}
+		   			it { should have_selector('input', value: 'Unfollow')}
+		   		end
+		   	end
+
+		   	describe "unfollowing a user" do
+		   		before do
+		   			user.follow!(other_user)
+		   			visit user_path(other_user)
+		   		end
+
+		   		it "should decrement the followed user count" do
+		   			expect do
+		   				click_button "Unfollow"
+		   			end.to change(user.followed_users, :count).by(-1)
+		   		end
+
+		   		it "should decrement the other user's followers count" do
+		   			expect do
+		   				click_button "Unfollow"
+		   			end.to change(other_user.followers, :count).by(-1)
+		   		end
+
+		   		describe "toggling the button" do
+		   			before {click_button "Unfollow"}
+		   			it {should have_selector('input', value: 'Follow')}
+		   		end
+		   	end
+	   end
+	end
 
    
 
